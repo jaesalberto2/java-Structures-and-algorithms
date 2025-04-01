8bf16c3edbb26b7d348f811a426be1090ff74386
jaesalberto2 <jaesalberto2@gmail.com>
2025-04-01 15:21:30 -0600

    append

--- linkedlists/src/LinkedList.java
+++ linkedlists/src/LinkedList.java
@@ -22,5 +22,39 @@ public class LinkedList {
       
     }
 
+    //print the linked list
+    public void printList() {
+        Node temp = head; // Start from the head
+        while (temp != null) { // While there are nodes in the list
+            System.out.println(temp.value); // Print the value of the current node
+            temp = temp.next; // Move to the next node
+        }
+        System.out.println("null"); // Print null at the end of the list
+    }
+
+    //getters
+    public Node getHead() {
+        return head; // Return the head of the list
+    }
+    public Node getTail() {
+        return tail; // Return the tail of the list
+    }
+    public int getLength() {
+        return length; // Return the length of the list
+    }
+
+    //append a new node to the end of the list
+    public void append(int value) {
+
+        Node newnode = new Node(value); // Create a new node with the given value
+        if (length == 0) { // If the list is empty
+            head = newnode; // Set the head to the new node
+            tail = newnode; // Set the tail to the new node
+        } else {
+            tail.next = newnode; // Set the next of the current tail to the new node
+            tail = newnode; // Update the tail to the new node
+        }
+        length++; // Increment the length of the list
+    }
 
 }
--- linkedlists/src/main.java
+++ linkedlists/src/main.java
@@ -1,6 +1,11 @@
 public class main {
+    public static void main(String[] args) {
    
-    LinkedList mLinkedList = new LinkedList(4); // Create a new linked list with a head node of value 10
+    LinkedList myLinkedList = new LinkedList(4); // Create a new linked list with a head node of value 10
 
-
+    myLinkedList.getHead(); // Get the head of the linked list
+    myLinkedList.getTail(); // Get the tail of the linked list
+    myLinkedList.getLength(); // Get the length of the linked list
+    myLinkedList.printList(); // Print the linked list
+    }
 }
