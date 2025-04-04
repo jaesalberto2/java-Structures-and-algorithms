f489d5864d8c5ac7ac577ccb4ca7d09fab7c202e
jaesalberto2 <jaesalberto2@gmail.com>
2025-04-03 20:50:52 -0600

    Graphs

---
+++ Graphs/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addEdge("A","B");
+        myGraph.addEdge("A","C");
+        myGraph.addEdge("B","C");
+        myGraph.printGraph();
+        myGraph.removeVertex("A");
+        myGraph.printGraph();
+    }
+}
